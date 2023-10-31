0239dbed8cbed5bc6589f1a6d24e289212ba5cdd
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2023-10-31 16:12:09 +0100

    deployment(CORS): add cors.json

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ;
 
     function loadTranslation(language) {
-        fetch(`/traductions/${language}.json`)
+        fetch(`https://github.com/d-perreaux/portfolio_v2/blob/main/traductions/${language}.json`)
             .then(response => response.json()
             .then(data => {
                 //DESCRIPTION
